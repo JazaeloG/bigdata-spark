4e2e58d874903a8d1cdf14bfca81157c7d21aeb2
Jazael Galindo <115960860+JazaeloG@users.noreply.github.com>
2023-03-20 19:42:09 -0600

    OctaveProgram

--- parcial1/proyectoparcial1/Proyecto.m
+++ parcial1/proyectoparcial1/Proyecto.m
@@ -1,72 +1,51 @@
-clear, clc
+clear;
+clc;
+
+
 %Definir patrones de entrada que representan las señales de peligro
 
 %Señal x
-X=[0 1 0 0 0 1 0;
-   0 0 1 0 1 0 0;
-   0 0 0 1 0 0 0;
-   0 0 1 0 1 0 0;
-   0 1 0 0 0 1 0;
-   0 0 0 0 0 0 0];
+X=[0 1 0 0 0 1 0;   0 0 1 0 1 0 0;   0 0 0 1 0 0 0;   0 0 1 0 1 0 0;   0 1 0 0 0 1 0;   0 0 0 0 0 0 0];
 
 %Señal Admiracion
-i=[0 0 0 1 0 0 0;
-   0 0 0 1 0 0 0;
-   0 0 0 1 0 0 0;
-   0 0 0 1 0 0 0;
-   0 0 0 0 0 0 0;
-   0 0 0 1 0 0 0];
+I=[0 0 0 1 0 0 0;   0 0 0 1 0 0 0;   0 0 0 1 0 0 0;   0 0 0 1 0 0 0;   0 0 0 0 0 0 0;   0 0 0 1 0 0 0];
 
 %SeñalRayo
-rayo=[0 0 0 0 0 1 0;
-      0 0 0 0 1 0 0;
-      0 0 0 1 0 0 0;
-      0 0 1 1 1 0 0;
-      0 0 0 1 0 0 0;
-      0 0 1 0 0 0 0];
+rayo=[0 0 0 0 0 1 0;      0 0 0 0 1 0 0;      0 0 0 1 0 0 0;      0 0 1 1 1 0 0;      0 0 0 1 0 0 0;      0 0 1 0 0 0 0];
 
 %Señal de viento
-viento=[0 1 0 1 0 1 0;
-        0 0 1 0 1 0 1;
-        0 1 0 1 0 1 0;
-        0 0 1 0 1 0 1;
-        0 1 0 1 0 1 0;
-        1 1 1 1 1 1 1];
+viento=[0 1 0 1 0 1 0;        0 0 1 0 1 0 1;        0 1 0 1 0 1 0;        0 0 1 0 1 0 1;        0 1 0 1 0 1 0;        1 1 1 1 1 1 1];
 
 %Señal C invertida
-c=[0 0 0 1 0 0 0;
-0 0 1 0 1 0 0;
-0 1 0 0 0 1 0;
-1 0 0 0 0 0 1;
-1 0 0 0 0 0 1;
-1 0 0 0 0 0 1];
+c=[0 0 0 1 0 0 0;   0 0 1 0 1 0 0;   0 1 0 0 0 1 0;   1 0 0 0 0 0 1;   1 0 0 0 0 0 1;   1 0 0 0 0 0 1];
 
 w0 = rayo(:) * rayo(:)';
 c2 = c(:) * c(:)';
 w2 = w0 + c2 + c2;
 
-u0 = c;
-ulast = c;
+u0 = c(:);
+ulast = c(:);
 d=1;
 
 while (1)
-u0 = u0 * w2;
+    w2 = w0 + 2*c2;
 
-for i=1:1:35
-if u0(i) > 0
-u0(i) = 1;
-else
-u0(i) = -1;
-endif
-endfor
 
-if (u0 == ulast)
-fprintf('Encontrado patrón %d \n', d);
-u0
-ulast
-break;
-endif
+    for i=1:1:35
+        if u0(i) > 0
+            u0(i) = 1;
+        else
+            u0(i) = -1;
+        end
+    end
 
-d = d + 1;
-ulast = u0;
+    if (isequal(u0, ulast))
+        fprintf('Encontrado patrón %d \n', d);
+        reshape(u0, 7, 6)'
+        ulast
+        break;
+    end
+
+    d = d + 1;
+    ulast = u0;
 end
